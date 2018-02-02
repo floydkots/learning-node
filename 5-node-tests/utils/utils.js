7722e6220dee3ab1f3b8b59f8d3d50196acbe555
Floyd Kots <floydkotohoyoh@gmail.com>
2018-02-02 18:20:55 +0300

    Installed expect and refactored tests to use assertions

--- 5-node-tests/utils/utils.js
+++ 5-node-tests/utils/utils.js
@@ -1,3 +1,10 @@
 module.exports.add = (a, b) => a + b;
 
 module.exports.square = (x) => x*x;
+
+module.exports.setName = (user, fullName) => {
+  let [firstName, lastName] = fullName.split(" ");
+  user.firstName = firstName;
+  user.lastName = lastName;
+  return user;
+};
--- 5-node-tests/utils/utils.test.js
+++ 5-node-tests/utils/utils.test.js
@@ -1,15 +1,42 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   let result = utils.add(33, 11);
-  if (result !== 44) {
-    throw new Error(`Expected 44, but got ${result}`)
-  }
+  expect(result).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
   let result = utils.square(9);
-  if (result !== 81) {
-    throw new Error(`Expected 81, but got ${result}`);
-  }
+  expect(result).toBe(81).toBeA('number');
+});
+
+it('should expect some values', () => {
+  // expect(12).toNotBe(11);
+  expect({name: 'Floyd'}).toEqual({name: 'Floyd'});
+});
+
+it('should include some values', () => {
+  expect([2, 3, 4]).toInclude(2);
+  expect([2, 3, 4]).toExclude(5);
+});
+
+it('should have some properties', () => {
+  expect({
+    name: 'Floyd',
+    age: 24,
+    location: 'Kenya'
+  }).toInclude({
+    age: 24
+  }).toExclude({
+    name: 'Peter'
+  })
+});
+
+
+it('should verify first and last names are set', () => {
+  let user = { location: 'Kenya' };
+  let result = utils.setName(user, 'Floyd Kots');
+  expect(result).toInclude({firstName: 'Floyd', lastName: 'Kots'});
 });
