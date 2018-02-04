19ec13be826c7c6d8690905cdc7a4ac0e6cb3bb2
Floyd Kots <floydkotohoyoh@gmail.com>
2018-02-04 12:28:48 +0300

    Wrote data to the mongodb instance

--- 6-node-todo-api/playground/mongodb-connect.js
+++ 6-node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
   if (error) {
@@ -16,16 +16,17 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
   //   console.log(JSON.stringify(result.ops, undefined, 2));
   // });
 
-  db.collection('Users').insertOne({
-    name: 'Floyd',
-    age: 24,
-    location: 'Kenya'
-  }, (error, result) => {
-    if (error) {
-      return console.log('Unable to insert user', error);
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-  });
+  // db.collection('Users').insertOne({
+  //   name: 'Floyd',
+  //   age: 24,
+  //   location: 'Kenya'
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert user', error);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
   db.close();
 });
