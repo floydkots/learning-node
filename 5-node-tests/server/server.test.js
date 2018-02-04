626ea86bc9f5aacc6c4e74781bf28d9eca4f04d9
Floyd Kots <floydkotohoyoh@gmail.com>
2018-02-04 08:53:35 +0300

    Use describe to group and organize the different tests

--- 5-node-tests/server/server.test.js
+++ 5-node-tests/server/server.test.js
@@ -2,29 +2,39 @@ const request = require('supertest');
 const expect = require('expect');
 const app = require('./server').app;
 
-it('should return hello world', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((response) => {
-      expect(response.body).toInclude({
-        error: 'Page not found.'
-      })
-    })
-    .end(done);
+describe('Server', () => {
+  describe('GET /', () => {
+    it('should return hello world', (done) => {
+      request(app)
+        .get('/')
+        .expect(404)
+        .expect((response) => {
+          expect(response.body).toInclude({
+            error: 'Page not found.'
+          })
+        })
+        .end(done);
+    });
+  });
+
+  describe('GET /users', () => {
+    it('should return an array of users', (done) => {
+      request(app)
+        .get('/users')
+        .expect(200)
+        .expect((response) => {
+          expect(response.body).toInclude(
+            { name: 'Floyd', age: 24 }
+          )
+            .toInclude(
+              { name: 'Kots', age: 25 }
+            )
+        })
+        .end(done)
+    });
+  });
 });
 
-it('should return an array of users', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((response) => {
-      expect(response.body).toInclude(
-        { name: 'Floyd', age: 24 }
-      )
-        .toInclude(
-          { name: 'Kots', age: 25 }
-        )
-    })
-    .end(done)
-});
+
+
+
--- 5-node-tests/utils/utils.test.js
+++ 5-node-tests/utils/utils.test.js
@@ -2,55 +2,68 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  let result = utils.add(33, 11);
-  expect(result).toBe(44).toBeA('number');
-});
+describe('Utils', () => {
 
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum)=> {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      let result = utils.add(33, 11);
+      expect(result).toBe(44).toBeA('number');
+    });
+
+    it('should async add two numbers', (done) => {
+      utils.asyncAdd(4, 3, (sum)=> {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+      })
+    });
+  });
+
+  describe('#square', () => {
+    it('should square a number', () => {
+      let result = utils.square(9);
+      expect(result).toBe(81).toBeA('number');
+    });
+
+    it('should async square a number', (done) => {
+      utils.asyncSquare(3, (square) => {
+        expect(square).toBe(9).toBeA('number');
+        done();
+      })
+    });
+  });
+
+  describe('#playground', () => {
+    it('should expect some values', () => {
+      // expect(12).toNotBe(11);
+      expect({name: 'Floyd'}).toEqual({name: 'Floyd'});
+    });
+
+    it('should include some values', () => {
+      expect([2, 3, 4]).toInclude(2);
+      expect([2, 3, 4]).toExclude(5);
+    });
+
+    it('should have some properties', () => {
+      expect({
+        name: 'Floyd',
+        age: 24,
+        location: 'Kenya'
+      }).toInclude({
+        age: 24
+      }).toExclude({
+        name: 'Peter'
+      })
+    });
+
+
+    it('should verify first and last names are set', () => {
+      let user = { location: 'Kenya' };
+      let result = utils.setName(user, 'Floyd Kots');
+      expect(result).toInclude({firstName: 'Floyd', lastName: 'Kots'});
+    });
   })
-});
 
-it('should square a number', () => {
-  let result = utils.square(9);
-  expect(result).toBe(81).toBeA('number');
-});
-
-it('should async square a number', (done) => {
-  utils.asyncSquare(3, (square) => {
-    expect(square).toBe(9).toBeA('number');
-    done();
-  })
-});
-
-it('should expect some values', () => {
-  // expect(12).toNotBe(11);
-  expect({name: 'Floyd'}).toEqual({name: 'Floyd'});
-});
-
-it('should include some values', () => {
-  expect([2, 3, 4]).toInclude(2);
-  expect([2, 3, 4]).toExclude(5);
-});
-
-it('should have some properties', () => {
-  expect({
-    name: 'Floyd',
-    age: 24,
-    location: 'Kenya'
-  }).toInclude({
-    age: 24
-  }).toExclude({
-    name: 'Peter'
-  })
 });
 
 
-it('should verify first and last names are set', () => {
-  let user = { location: 'Kenya' };
-  let result = utils.setName(user, 'Floyd Kots');
-  expect(result).toInclude({firstName: 'Floyd', lastName: 'Kots'});
-});
+
