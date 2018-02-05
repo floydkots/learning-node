74d7995bca090ab05fba4e704256ae8f6d7a3466
Floyd Kots <floydkotohoyoh@gmail.com>
2018-02-05 14:08:01 +0300

    Added the GET /todos/:id route

--- 6-node-todo-api/server/server.js
+++ 6-node-todo-api/server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -27,6 +28,23 @@ app.get('/todos', (request, response) => {
       response.send({ todos });
     }, (error) => {
       response.status(400).send(error);
+    })
+});
+
+app.get('/todos/:id', (request, response) => {
+  const id = request.params.id;
+  if (!ObjectID.isValid(id)) {
+    return response.status(404).send();
+  }
+  Todo.findById(id)
+    .then((todo) => {
+      if (!todo) {
+        return response.status(404).send()
+      }
+      response.send(todo);
+    })
+    .catch((error) => {
+      return response.status(400).send()
     })
 });
 
