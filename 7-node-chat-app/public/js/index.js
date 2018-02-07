ed9085b1f6cb447f2e1fb23880904c9d8dc667f9
Floyd Kots <floydkotohoyoh@gmail.com>
2018-02-07 14:09:00 +0300

    Emit newMessage on createMessage

--- 7-node-chat-app/public/js/index.js
+++ 7-node-chat-app/public/js/index.js
@@ -3,13 +3,7 @@ let socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    to: '@floyd',
-    from: '@kots',
-    text: 'Come on in!'
-  });
 });
-
 
 socket.on('newMessage', function(message) {
   console.log('Got new message', message);
