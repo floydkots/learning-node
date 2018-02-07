b42390253240f445dd26be200e6d7b5517d16a36
Floyd Kots <floydkotohoyoh@gmail.com>
2018-02-07 14:38:51 +0300

    Greet new user and alert others

--- 7-node-chat-app/public/js/index.js
+++ 7-node-chat-app/public/js/index.js
@@ -2,11 +2,19 @@ let socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
 });
+
 
 socket.on('newMessage', function(message) {
   console.log('Got new message', message);
+});
+
+socket.on('welcomeMessage', function(message) {
+  console.log('Got welcome message', message);
+});
+
+socket.on('broadcastMessage', function(message) {
+  console.log('Got broadcast message', message);
 });
 
 socket.on('disconnect', function() {
