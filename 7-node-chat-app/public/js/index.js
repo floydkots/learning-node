d30e431d84a8ca50c59650398d6dbd5d445605c3
Floyd Kots <floydkotohoyoh@gmail.com>
2018-02-07 21:06:13 +0300

    Scroll to bottom if user is close to bottom

--- 7-node-chat-app/public/js/index.js
+++ 7-node-chat-app/public/js/index.js
@@ -1,5 +1,20 @@
 let socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  let messages = jQuery('#messages');
+  let newMessage = messages.children('li:last-child');
+  // Heights
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('Connected to server');
 });
@@ -14,7 +29,7 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
-
+  scrollToBottom();
 });
 
 socket.on('disconnect', function() {
@@ -30,6 +45,7 @@ socket.on('newLocationMessage', function(message) {
     url: message.url
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 let messageTextbox = jQuery('[name=message]');
